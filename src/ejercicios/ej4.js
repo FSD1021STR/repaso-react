248fc7cd19f8422eb3cedbb1665b9f4427b811e4
JGamito <gamito90@hotmail.com>
2021-12-01 22:32:05 +0100

    Refresh

--- src/ejercicios/ej4.js
+++ src/ejercicios/ej4.js
@@ -1,1 +1,16 @@
 import React, { useState } from "react";
+
+const Ej4 = () => {
+  return (
+    <div>
+      <div className="father">
+        <div className="child"></div>
+      </div>
+
+      <div className="father">
+        <div className="child"></div>
+      </div>
+    </div>
+  );
+};
+export default Ej4;
