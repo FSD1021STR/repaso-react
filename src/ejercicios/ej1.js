1d924bea8f6f933c4dfc31f308219d41d2d944c2
Ion Sora <soraioniosif@gmail.com>
2021-12-11 19:14:12 +0100

    Añadido botón 'quitar 1', creado useState

--- src/ejercicios/ej1.js
+++ src/ejercicios/ej1.js
@@ -11,15 +11,23 @@ const Ej1 = () => {
   const handleOnClickReset = () => {
     setContador(0);
   };
+  const handleOnClickQuitar1 =() => {
+    setContador( contador-1);
+  }
 
   return (
     <div>
       <h2>Cuantos clicks puedes dar antes de cansarte?</h2>
+      <div className='nom'>
       <label>{contador}</label>
+      </div>
+      <div>
       <button className="counter" onClick={handleOnClickContador}>
         Click!
       </button>
       <button className="reset" onClick={handleOnClickReset}>reset!</button>
+      <button className='quita' onClick={handleOnClickQuitar1}>Quita 1</button>
+      </div>
     </div>
   );
 };
