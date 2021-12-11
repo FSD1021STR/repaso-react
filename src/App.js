95389ab3629244e8785b2f2c662d6ff7285ad7de
Ion Sora <soraioniosif@gmail.com>
2021-12-11 19:16:25 +0100

    Cambiado el numero de ejercicio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Ej9 from './ejercicios/ej9';
+import Ej1 from './ejercicios/ej1';
 
 function App() {
   return (
     <div >
-      <Ej9 />
+      <Ej1 />
     </div>
   );
 }
